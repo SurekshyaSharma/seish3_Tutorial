e60e682660a3b2e6259c61753b90bac66658e4be
SurekshyaSharma <shar3415@stthomas.edu>
2020-10-02 22:46:21 -0500

    First Failed TDD

--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -8,18 +8,9 @@ public class ValidateISBNTest {
 
 	@Test
 	public void checkAValidISBN() {
-		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN("0140449116");
-		assertTrue("first value", result);
-		result = validator.checkISBN("0140177396");
-		assertTrue("second value", result);
+		fail("Not yet implemented");
 	}
 	
-	@Test
-	public void checkAnInvalidISBN() {
-		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN("0140449117");
-		assertFalse(result);
-	}
+
 
 }
